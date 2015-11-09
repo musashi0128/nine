f687da702d7c701262ba75db3d03f3050d8f6ffd
kazuhikomiyamoto <k.miyamoto.0128@gmail.com>
2015-11-09 23:54:16 +0000

    11/10

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,22 @@
 class StaticPagesController < ApplicationController
   def home
   end
+  
+  def about
+  end
+  
+  def qa
+  end
+  
+  def produce
+  end
+  
+  def news
+  end
+  
+  def business
+  end
+  
+  def designer
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
+  
+  get 'qa'    => 'static_pages#qa'
+  get 'about'   => 'static_pages#about'
+  get 'designer' => 'static_pages#designer'
+  get 'produce'    => 'static_pages#produce'
+  get 'news'   => 'static_pages#news'
+  get 'business' => 'static_pages#business'
 end
