a048195f74d9ea95cf0843ccb0ada288bdf9eb27
kazuhikomiyamoto <k.miyamoto.0128@gmail.com>
2015-11-13 13:14:01 +0000

    up

---
+++ app/controllers/designers_controller.rb
@@ -0,0 +1,29 @@
+class DesignersController < ApplicationController
+  def torafu
+  end
+  
+  def fujimto
+  end
+  
+  def nakamurah 
+  end
+  
+  def nakamurar
+  end
+  
+  def nagayama
+  end
+  
+  def hirata
+  end
+  
+  def igarashi
+  end
+  
+  def oonishi
+  end
+  
+  def yoshimura
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
   
+  #static系
   get 'qa'    => 'static_pages#qa'
   get 'about'   => 'static_pages#about'
   get 'designer' => 'static_pages#designer'
   get 'produce'    => 'static_pages#produce'
   get 'news'   => 'static_pages#news'
   get 'business' => 'static_pages#business'
+  
+  
+  #designer系
+  get 'torafu' => 'designers#torafu'
+  get 'fujimoto' => 'designers#fujimoto'
+  get 'nakamurah' => 'designers#nakamurah'
+  get 'nakamurar' => 'designers#nakamurar'
+  get 'nagayama' => 'designers#nagayama'
+  get 'hirata' => 'designers#hirata'
+  get 'igarashi' => 'designers#igarashi'
+  get 'oonishi' => 'designers#oonishi'
+  get 'yoshimura' => 'designers#yoshimura'
+  get 'tanijiri' => 'designers#tanijiri'
 end
