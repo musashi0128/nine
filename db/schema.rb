ed8cf0986bca9a27c907aa7db3f381830fc31b00
kazuhikomiyamoto <k.miyamoto.0128@gmail.com>
2015-11-15 00:05:58 +0000

    finish!

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151114225850) do
+
+  create_table "maps", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
